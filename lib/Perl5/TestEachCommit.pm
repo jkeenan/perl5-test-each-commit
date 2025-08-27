12092757f602034859d8351746f6841bc2987967
James E Keenan <jkeenan@cpan.org>
2025-08-27 19:50:48 -0400

    Begin to write documentation

--- lib/Perl5/TestEachCommit.pm
+++ lib/Perl5/TestEachCommit.pm
@@ -1,51 +1,12 @@
 package Perl5::TestEachCommit;
-use strict;
+use 5.14;
+use warnings;
+our $VERSION = '0.01';
+use Carp;
+#use IO::File;
+#use IO::Zlib;
 
-BEGIN {
-    use Exporter ();
-    use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
-    $VERSION     = '0.01';
-    @ISA         = qw(Exporter);
-    #Give a hoot don't pollute, do not export more than needed by default
-    @EXPORT      = qw();
-    @EXPORT_OK   = qw();
-    %EXPORT_TAGS = ();
-}
-
-
-#################### subroutine header begin ####################
-
-=head2 sample_function
-
- Usage     : How to use this function/method
- Purpose   : What it does
- Returns   : What it returns
- Argument  : What it wants to know
- Throws    : Exceptions and other anomolies
- Comment   : This is a sample subroutine header.
-           : It is polite to include more pod and fewer comments.
-
-See Also   :
-
-=cut
-
-#################### subroutine header end ####################
-
-
-sub new
-{
-    my ($class, %parameters) = @_;
-
-    my $self = bless ({}, ref ($class) || $class);
-
-    return $self;
-}
-
-
-#################### main pod documentation begin ###################
-## Below is the stub of documentation for your module.
-## You better edit it!
-
+=encoding utf8
 
 =head1 NAME
 
@@ -53,9 +14,9 @@ Perl5::TestEachCommit - Test each commit in a pull request to Perl core
 
 =head1 SYNOPSIS
 
-  use Perl5::TestEachCommit;
-  blah blah blah
+    use Perl5::TestEachCommit;
 
+    my $self = Perl5::TestEachCommit->new();
 
 =head1 DESCRIPTION
 
@@ -65,18 +26,11 @@ to leave the stub unedited.
 
 Blah blah blah.
 
-
 =head1 USAGE
-
-
 
 =head1 BUGS
 
-
-
 =head1 SUPPORT
-
-
 
 =head1 AUTHOR
 
@@ -93,15 +47,11 @@ it and/or modify it under the same terms as Perl itself.
 The full text of the license can be found in the
 LICENSE file included with this module.
 
-
 =head1 SEE ALSO
 
 perl(1).
 
 =cut
-
-#################### main pod documentation end ###################
-
 
 1;
 # The preceding line will help the module return a true value
